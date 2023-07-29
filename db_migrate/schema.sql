5acaf5fc2bb4eb21c191fff79052449829c9dbdc
shaninalex <shanin.x@gmail.com>
2023-07-29 15:19:18 +0300

    update db

--- db_migrate/schema.sql
+++ db_migrate/schema.sql
@@ -1,7 +1,21 @@
 CREATE EXTENSION "uuid-ossp";
 
-CREATE TABLE "users"
-(
+CREATE TABLE "providers" (
+    "id" SERIAL,
+    "name" VARCHAR(50) NOT NULL,
+    PRIMARY KEY ("id")
+);
+
+-- "local" - default provider for user created in the system by email/password
+INSERT INTO
+    "providers" ("name")
+VALUES
+    ('local'),
+    ('google'),
+    ('facebook'),
+    ('twitter');
+
+CREATE TABLE "users" (
     "id" uuid DEFAULT uuid_generate_v4(),
     "name" VARCHAR(256) NOT NULL,
     "email" VARCHAR(256) NOT NULL UNIQUE,
@@ -9,4 +23,12 @@ CREATE TABLE "users"
     "image" TEXT,
     "created_at" TIMESTAMP DEFAULT current_timestamp,
     PRIMARY KEY ("id")
+);
+
+CREATE TABLE "user_providers" (
+    "id" SERIAL,
+    "user_id" uuid NOT NULL,
+    "provider_id" INTEGER NOT NULL,
+    FOREIGN KEY ("user_id") REFERENCES "users" ("id"),
+    FOREIGN KEY ("provider_id") REFERENCES "providers" ("id")
 );
