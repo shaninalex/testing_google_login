ec2a8f74ae6c7f4c70e13fcc41539d0df8ab66b5
shaninalex <shanin.x@gmail.com>
2023-07-29 22:09:27 +0300

    validate token

--- backend/jwt.go
+++ backend/jwt.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"time"
 
@@ -14,10 +15,9 @@ func CreateJWT(id, email string) (string, error) {
 	claims := token.Claims.(jwt.MapClaims)
 
 	claims["sub"] = id
-	claims["email"] = email
 	claims["exp"] = time.Now().Add(time.Minute * 30).Unix()
 
-	tokenString, err := token.SignedString(os.Getenv("SECRET_KEY"))
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET_KEY")))
 
 	if err != nil {
 		fmt.Printf("Something Went Wrong: %s", err.Error())
@@ -25,3 +25,56 @@ func CreateJWT(id, email string) (string, error) {
 	}
 	return tokenString, nil
 }
+
+func validateToken(t string) (string, error) {
+	token, err := jwt.Parse(t, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("There was an error in parsing")
+		}
+		return []byte(os.Getenv("SECRET_KEY")), nil
+	})
+
+	if err != nil {
+		log.Println(err)
+		log.Println("error on parsing")
+		return "", err
+	}
+
+	if token == nil {
+		log.Println(err)
+		log.Println("invalid token")
+		return "", err
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		log.Println("couldn't parse claims")
+		return "", err
+	}
+
+	exp := claims["exp"].(float64)
+	if int64(exp) < time.Now().Local().Unix() {
+		log.Println("token expired")
+		return "", err
+	}
+
+	return claims["sub"].(string), err
+}
+
+// if token == nil {
+// 	fmt.Fprintf(w, "invalid token")
+// 	return errors.New("Token error")
+// }
+
+// claims, ok := token.Claims.(jwt.MapClaims)
+// if !ok {
+// 	fmt.Fprintf(w, "couldn't parse claims")
+// 	return errors.New("Token error")
+// }
+
+// exp := claims["exp"].(float64)
+// if int64(exp) < time.Now().Local().Unix() {
+// 	fmt.Fprintf(w, "token expired")
+// 	return errors.New("Token error")
+// }
+
+// return nil
