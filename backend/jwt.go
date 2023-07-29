3d33aa7e8fbda767acc6d248d1bca82ce7e6d481
shaninalex <shanin.x@gmail.com>
2023-07-29 19:07:27 +0300

    generate token

--- backend/jwt.go
+++ backend/jwt.go
@@ -1,27 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"time"
 
-	"github.com/golang-jwt/jwt"
+	"github.com/golang-jwt/jwt/v4"
 )
 
 func CreateJWT(id, email string) (string, error) {
-	// tokenValidityDuration := 24 * time.Hour
-	// now := time.Now()
-	// exp := now.Add(tokenValidityDuration)
-	token := jwt.New(jwt.SigningMethodEdDSA)
+	// https://www.golinuxcloud.com/golang-jwt/
+	token := jwt.New(jwt.SigningMethodHS256)
 	claims := token.Claims.(jwt.MapClaims)
-	claims["exp"] = time.Now().Add(24 * time.Hour)
-	claims["authorized"] = true
-	claims["user"] = "username"
+
+	claims["sub"] = id
+	claims["email"] = email
+	claims["exp"] = time.Now().Add(time.Minute * 30).Unix()
 
 	tokenString, err := token.SignedString(os.Getenv("SECRET_KEY"))
+
 	if err != nil {
+		fmt.Printf("Something Went Wrong: %s", err.Error())
 		return "", err
 	}
-
 	return tokenString, nil
-
 }
